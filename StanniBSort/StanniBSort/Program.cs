75c4eaf620ec853a776ebb5b7593e59df6ddeaba
Slew401 <nv20676@bristol.ac.uk>
2022-03-15 10:15:22 +0000

    Merge Method Addition

--- StanniBSort/StanniBSort/Program.cs
+++ StanniBSort/StanniBSort/Program.cs
@@ -6,17 +6,42 @@ public class BubbleSort
 {
     public static void Main()
     {
-        int[] arr = new int[]{ 12, 32, 31, 45, 15, 96, 70 };
-        BSort(arr);
-        foreach(int i in arr)
-        {
-            Console.WriteLine(i);
-        }
+        int[] arr = new int[]{ 5,4,3,2,1 };
+        BubSort(arr);
+        List<int> list = new() { 1,3,5,7,9};
+        List<int> list2 = new() { 2,4,6,8,10 };
+
+        var mergedList = mergeMethod(list, list2);
+        //foreach(int i in arr)
+        //{
+        //    Console.WriteLine(i);
+        //}
+
+        mergedList.ForEach(x => Console.Write($"{x} "));
+
+
     }
 
+    private static List<int> mergeMethod(List<int> list, List<int> list2)
+    {
+        List<int> result = new();
 
-    // Intiution behind BSort is simple for each value
-    public static void BSort(int[] array)
+        foreach(int i in list)
+        {
+            result.Add(i);
+        }
+
+        foreach(int item in list2)
+        {
+            result.Add(item);
+        }
+
+        BubSort(result);
+
+        return result; 
+    }
+
+    public static void BubSort(int[] array)
     {
         int temp = 0;
 
@@ -33,4 +58,24 @@ public class BubbleSort
             }
         }
     }
+
+    public static void BubSort(List<int> array)
+    {
+        int temp = 0;
+
+        for (int i = 0; i < array.Count; i++)
+        {
+            for (int j = i + 1; j < array.Count; j++)
+            {
+                if (array[i] > array[j])
+                {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
+
 }
+    //public static List<int> Bub
